a7f1364fd40c5558cbaa1ffba004d33db24f3418
adelaide20 <adelaiderorisang@gmail.com>
2024-02-09 09:01:06 +0200

    second commit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { SplashComponent } from './pages/splash/splash.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component:SplashComponent},
+  {path: '404', component:NotFoundComponent},
+  {path: '**', component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashComponent } from './pages/splash/splash.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
