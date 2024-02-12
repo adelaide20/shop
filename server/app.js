28d16c1c230baddaa2f701054f40eb3cac593451
adelaide20 <adelaiderorisang@gmail.com>
2024-02-12 08:48:04 +0200

    basic route

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+const bodyParser = require("body-parser");
+const dotenv = require('dotenv')
+
+const port = process.env.PORT || 3000
+
+dotenv.config();
+
+var cors = require('cors')
+
+
+var corsOptions = {
+    origin: '*',
+    allowCredentials: "true",
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(express.json())
+
+app.use(cors(corsOptions))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// basic route for testing
+app.get('/', (req, res) => res.send('Hello, Welcome To Sneaker App'))
+
+
+app.listen(port, () => console.log(`Example app listening on port  ${port}`))
